15e3065074f470abb05fe372fda49f9c7bb858c2
aatwater <atwater.a@gmail.com>
2016-10-14 17:53:55 -0500

    Add files via upload

---
+++ RCommander_SVP_002.R
@@ -0,0 +1,108 @@
+
+load("/Users/amyatwater/Desktop/SVP_2016.csv")
+scatterplot(m1.W~m1.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Lower First Molar Length", ylab="Lower First Molar Width", 
+  main="Lower First Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(M1.W~M1.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper First Molar Length", ylab="Upper First Molar Width", 
+  main="Upper First Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(M2.W~M2.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper First Molar Length", ylab="Upper First Molar Width", 
+  main="Upper First Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(M2.W~M2.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper Second Molar Length", ylab="Upper Second Molar Width", 
+  main="Upper Second Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(m2.W~m2.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Lower Second Molar Length", ylab="Lower Second Molar Width", 
+  main="Lower Second Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(m3.W~m3.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Lower Third Molar Length", ylab="Lower Third Molar Width", 
+  main="Lower Third Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(M3.W~M3.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper Third Molar Length", ylab="UpperThird Molar Width", 
+  main="Upper Third Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(p3.W~p3.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper Third Molar Length", ylab="UpperThird Molar Width", 
+  main="Upper Third Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(p4.W~p4.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper Third Molar Length", ylab="UpperThird Molar Width", 
+  main="Upper Third Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(p4.W~p4.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Lower Forth Premolar Length", ylab="Lower Forth Premolar Width", 
+  main="Lower Forth preMolar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(p4.W~p4.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Lower Fourth Premolar Length", ylab="Lower Fourth Premolar Width", 
+  main="Lower Fourth Premolar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(P3.W~P3.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper Third Premolar Length", ylab="Upper Third Premolar Width", 
+  main="Upper Third Premolar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(P4.W~P4.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper Fourth Premolar Length", ylab="Upper Fourth Premolar Width", 
+  main="Upper Fourth Premolar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(p4.W~p4.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Lower Fourth Premolar Length", ylab="Lower Fourth Premolar Width", 
+  main="Lower Fourth Premolar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(m1.W~m1.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Lower First Molar Length", ylab="Lower First Molar Width", 
+  main="Lower First Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(M1.W~M1.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Lower Second Molar Length", ylab="Lower Second Molar Width", 
+  main="Lower Second Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(M1.W~M1.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper First Molar Length", ylab="Upper First Molar Width", 
+  main="Upper First Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(M1.W~M1.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper Second Molar Length", ylab="Upper Second Molar Width", 
+  main="Upper Second Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(M1.W~M1.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper Second Molar Length", ylab="Upper Second Molar Width", 
+  main="Upper Second Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(M2.W~M2.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper Second Molar Length", ylab="Upper Second Molar Width", 
+  main="Upper Second Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+scatterplot(M3.W~M3.L | Taxa, reg.line=FALSE, smooth=FALSE, spread=FALSE, 
+  boxplots=FALSE, span=0.5, ellipse=FALSE, levels=c(.5, .9), 
+  xlab="Upper Third Molar Length", ylab="Upper Third Molar Width", 
+  main="Upper Third Molar Dimensions", by.groups=TRUE, 
+  legend.coords="bottomright", data=fossil)
+
